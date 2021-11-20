799266055f7e87e33c3bf96cc91f4a1d148ed578
ZnataX <nata.91@op.pl>
2021-11-20 11:01:02 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 //        for (int liczba:tablica) {
 //            if (liczba > 3 & liczba < 5)
 //            System.out.println(liczba);
+//        ------------------------------------------
 //        int day = 4;
 //        switch (day){
 //            case 1:
@@ -18,24 +19,35 @@ public class Main {
 //            default:
 //                System.out.println("Weekend");
 //        }
+//        --------------------------------------------------
 
-        int i = 0;
-        while (i < 5){
-            System.out.println(i);
-            i++;
+//        int i = 0;
+//        while (i < 5){
+//            System.out.println(i);
+//            i++;
+//        }
+//        ------------------------------------------------------
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
         }
+        while (j < 5);
         }
+//        ---------------------------------
 //        for(int i = 0; i < tablica.length; i++){
 //            if( tablica[i] < 3){
 //                System.out.println("True");
 //            } else if (tablica[i] < 5){
 //        System.out.println("False");}
 //    }
+//    ---------------------------------------------
 
 //        String [] cars = {"audi", "bmw", "vw", "volvo"};
 //        for(String car:cars){
 //            System.out.println(car);
 //        }
+//    --------------------------------------------------
 
             }
 
