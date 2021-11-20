621e953b481323c85c419445939bf3ff3496a092
ZnataX <nata.91@op.pl>
2021-11-20 10:57:24 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        int [] tablica = {6,5,4,3,2,1};
-        for(int i = 0; i < tablica.length; i++){
-            if( tablica[i] < 3){
-                System.out.println("True");
-            } else if (tablica[i] < 5){
-                System.out.println("False");}
+//       int [] tablica = {6,5,4,3,2,1};
+//        for (int liczba:tablica) {
+//            if (liczba > 3 & liczba < 5)
+//            System.out.println(liczba);
+//        int day = 4;
+//        switch (day){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            default:
+//                System.out.println("Weekend");
+//        }
+
+        int i = 0;
+        while (i < 5){
+            System.out.println(i);
+            i++;
+        }
+        }
+//        for(int i = 0; i < tablica.length; i++){
+//            if( tablica[i] < 3){
+//                System.out.println("True");
+//            } else if (tablica[i] < 5){
+//        System.out.println("False");}
+//    }
+
+//        String [] cars = {"audi", "bmw", "vw", "volvo"};
+//        for(String car:cars){
+//            System.out.println(car);
+//        }
 
             }
 
-        }
-    }
+
+
 
