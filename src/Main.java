c14fa9ecb921c0a0c0e3d295e8f7147f818101fa
ZnataX <nata.91@op.pl>
2021-11-20 10:00:13 +0100

    tablica_zadanie

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] tablica = {6,5,4,3,2,1};
+        for(int i = 0; i < tablica.length; i++){
+            if( tablica[i] < 3){
+                System.out.println("True");
+            } else if (tablica[i] < 5){
+                System.out.println("False");}
+            }
+
+        }
+    }
+
